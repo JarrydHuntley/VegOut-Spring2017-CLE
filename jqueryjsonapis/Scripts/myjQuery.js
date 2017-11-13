693e5d20d534725d8f47cb53f23883227c01137a
Jarryd Huntley <Jarryd Huntley>
2017-11-13 10:32:32 -0500

    Added in the alert dialog.

--- jqueryjsonapis/Scripts/myjQuery.js
+++ jqueryjsonapis/Scripts/myjQuery.js
@@ -1,1 +1,39 @@
-﻿
+﻿//$(function() {
+$(document).ready(function () {
+
+    $("#findButton").click(function () {
+        console.log("Find Button was clicked.");
+        var searchTerm = $("#veggieInput").val();
+
+        var listItems = $(".list-group li");
+
+        //$(".list-group li").effect("shake", { times: 4 }, 1000)
+
+        listItems.each(function () {
+            if (!$(this).text().includes(searchTerm)) {
+                $(this).hide("slow");
+            }
+        });
+
+        if (!$("list-group").text().includes(searchTerm)) {
+            $("#dialogText").text("Please enter a recipe");
+           $("#dialog").dialog('open');
+        }
+    });
+
+    $("#viewAllButton").click(function () {
+        console.log("View all button was clicked");
+
+        $(".list-group li").show("fast");
+    });
+
+
+    $("#dialog").dialog({
+        modal: true,
+        autoOpen: false,
+        title: "Add new recipe",
+        width: 400,
+        height: 200
+    });
+
+});
